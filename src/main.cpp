02a2ae60e85a4516a6faeb75da9092535a9282cc
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2025-04-22 12:39:36 +0300

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+
+#define BX_CONFIG_DEBUG 1
+#include "bx/bx.h"
+#include "bgfx/bgfx.h"
+#include "bgfx/platform.h"
+
+#include "GLFW/glfw3.h"
+
+#define GLFW_EXPOSE_NATIVE_X11
+#include "GLFW/glfw3native.h"
+
+const int W_WIDTH = 800;
+const int W_HEIGHT = 600;
+
+
+static void glfw_errorCallback(int error, const char *description)
+{
+    fprintf(stderr, "GLFW error, %d: %s\n", error, description);
+}
+
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+int main (int argc, char* args[]) 
+{
+    std::cout << "Starting the engine..." << std::endl;
+    glfwSetErrorCallback(glfw_errorCallback);
+
+    glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_X11);
+    if (!glfwInit())
+    {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        return 1;
+    }
+
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+    GLFWwindow* window = glfwCreateWindow(W_WIDTH, W_HEIGHT, "Galaxy-engine", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    // Call bgfx::renderFrame before bgfx::init to signal to bgfx not to create a render thread.
+	// Most graphics APIs must be used on the same thread that created the window.
+    bgfx::renderFrame();
+
+
+    bgfx::PlatformData pd;
+    pd.ndt = glfwGetX11Display();
+    pd.nwh = (void *)(uintptr_t) glfwGetX11Window(window);
+
+    bgfx::Init init;
+    init.type = bgfx::RendererType::OpenGL;
+    init.resolution.width = W_WIDTH;
+    init.resolution.height = W_HEIGHT;
+    init.resolution.reset = BGFX_RESET_VSYNC;
+    init.platformData = pd;
+
+    if (bgfx::init(init) != true)
+    {
+        std::cout << "Failed to initialize bgfx" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    bgfx::setDebug(BGFX_DEBUG_TEXT);
+
+
+    while(!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glfwPollEvents();    
+        glfwSwapBuffers(window);
+    }
+
+
+    bgfx::shutdown();
+    glfwTerminate();
+    return 0;
+}
+
